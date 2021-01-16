a41578ef2e901974332eafac51fd89401e9c8a23
tokq <melindachang.hy@gmail.com>
2021-01-16 11:39:56 -0800

    basic slide setup

--- src/main.js
+++ src/main.js
@@ -1,23 +1,9 @@
-import { createApp } from 'lucia/dist/legacy/lucia.esm';
+const Lucia = require('lucia/dist/legacy/lucia.cjs');
 
 const mainApp = () => {
-  const state = {
-    value: '',
-    output: '',
-    history: JSON.parse(localStorage.__guru_history ?? '[]'),
-    async submit() {
-      // const res = await fetch('someurl');
-      // const body = await res.json();
-
-      // this.output = body.data; // arbitrary prop ...
-      this.output = 'we dont know yet :(';
-      this.history.push(this.value);
-      this.value = '';
-      localStorage.__guru_history = JSON.stringify(this.history);
-    },
-  };
-
-  createApp(state).mount('#app');
+  Lucia.createApp({
+    slide: 0
+  }).mount('#app');
 };
 
 mainApp();
