8e2bc5e56d058f279d8acad55dbc4c24136e6378
tokq <melindachang.hy@gmail.com>
2021-01-16 23:38:23 -0800

    add: simple transition btwn pages

--- src/main.js
+++ src/main.js
@@ -20,3 +20,33 @@ textrev.from(".line h1", {
   },
   duration: 0.9
 });
+
+const slideSwap = gsap.timeline({
+  paused: true
+});
+
+slideSwap.fromTo('.slideSwap', {
+  x: '0'
+}, {
+  x: '100%',
+  duration: 1,
+  ease: "power4.out",
+  delay: 0.2
+}).to('.slideSwap', {
+  x: '200%',
+  duration: 1,
+  ease: "power4.out",
+  delay: 0.5
+});
+
+let hitbox = document.querySelectorAll('.hitbox');
+let startArrow = document.querySelector('.arrow');
+startArrow.addEventListener('click', function () {
+  slideSwap.restart()
+})
+
+hitbox.forEach(function (item) {
+  item.addEventListener('click', function () {
+    slideSwap.restart()
+  })
+})
