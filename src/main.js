b0d6db3a800d404689a1190cf42a8eb696912ae4
tokq <melindachang.hy@gmail.com>
2021-01-16 20:37:33 -0800

    First page minus animations and splash vid

--- src/main.js
+++ src/main.js
@@ -7,3 +7,15 @@ const mainApp = () => {
 };
 
 mainApp();
+
+// const textrev = gsap.timeline();
+
+// textrev.from(".line span", 1.8, {
+//   y: 200,
+//   ease: "power4.out",
+//   delay: 1,
+//   skewY: 10,
+//   stagger: {
+//     amount: 0.4,
+//   },
+// });
