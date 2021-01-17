452de30d2b14f97fddc45404516e854c13de2b7d
Will Lane <williamlane923@gmail.com>
2021-01-17 13:59:46 -0800

    added base odometer still need to add styling

--- src/main.js
+++ src/main.js
@@ -5,6 +5,7 @@ import {
 createApp({
   slide: 0,
   value: ' ',
+  odometer_value: 230,
   submit() {
     const headers = new Headers();
     headers.append('Content-Type', 'application/json');
@@ -25,6 +26,7 @@ createApp({
       .then((result) => console.log(result))
       .catch((error) => console.log('error', error));
   },
+
 }).mount('#app');
 
 const textrev = gsap.timeline();
@@ -99,7 +101,10 @@ slideTwoScroll.to('.n1', {
   y: '-100%',
   delay: 7,
   ease: 'power2.inOut', // transition from 1st
-  duration: 1
+  duration: 1,
+  onComplete: () => {
+    paypal.update(25286)
+  }
 }).to('.n2', {
   // opacity: 1,
   y: '-100%',
@@ -110,7 +115,10 @@ slideTwoScroll.to('.n1', {
   delay: 6,
   y: '-200%',
   ease: 'power2.inOut', //transition from 2nd
-  duration: 1
+  duration: 1,
+  onComplete: () => {
+    shakespear.update(17121)
+  }
 }).to('.n3', {
   // opacity: 1,
   y: '-200%',
@@ -142,5 +150,33 @@ slideTwoOpacity.to('.n1', {
 
 document.querySelector('[l-if="slide === 1"] .mainBtn .hitbox').addEventListener('click', function () {
   slideTwoScroll.restart();
+  setTimeout(() => {
+    airbnb.update(19363)
+  }, 1500)
   slideTwoOpacity.restart();
 });
+
+
+let airbnb = new Odometer({
+  el: document.querySelector("#app > div:nth-child(3) > div > section.reveal.n1 > div > h1"),
+  value: 0,
+
+  format: '',
+  theme: 'minimal'
+});
+
+let paypal = new Odometer({
+  el: document.querySelector("#app > div:nth-child(3) > div > section.reveal.n2 > div > h1"),
+  value: 0,
+
+  format: '',
+  theme: 'minimal'
+})
+
+let shakespear = new Odometer({
+  el: document.querySelector("#app > div:nth-child(3) > div > section.reveal.n3 > h2:nth-child(2)"),
+  value: 25286,
+
+  format: '',
+  theme: 'minimal'
+})
